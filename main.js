eb6772cc3d8f86861c35df5d9808235a701ca340
emmabennett <emma.j.bennett15@gmail.com>
2018-05-28 11:02:36 -0700

    finished portfolio

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
+
+// modal
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
   });
+  
+// testimonials 
+  var quoteCounter = 0;
+  var quoteLength = $('blockquote').length;
+
+  setInterval(function () {
+    $('blockquote').eq(quoteCounter).fadeOut(800, function () {
+      if (quoteCounter === quoteLength - 1) {
+        quoteCounter = 0;
+      } else {
+        quoteCounter += 1;
+      };
+      $('blockquote').eq(quoteCounter).fadeIn();
+    });
+
+  }, 7000);
